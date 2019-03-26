fa8ce9671c26ecc737890c74d06f5aa0ce159cbd
Shirley Keter <shirleyketer@gmail.com>
2019-03-26 03:29:00 +0300

    add details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,10 @@ import { Quote } from './quote';
 
 export class AppComponent {
     quotes = [
-        new Quote(1,'Only the good die young','posteby Billy Joel',),
-        new Quote(2,'Nitaambia watu nini','by Dj Shiti',),
-        new Quote(3, 'Only dead fish go with the flow','by Anny',),
-        new Quote(4, 'No guts no story','by Chris Brady',),
+        new Quote(1,'Only the good die young','posted by Billy Joel',),
+        new Quote(2,'Nitaambia watu nini','posted by Dj Shiti',),
+        new Quote(3, 'Only dead fish go with the flow','posted by Anny',),
+        new Quote(4, 'No guts no story','posted by Chris Brady',),
     
     ]
    
